813348d47efec6acd01a18f4c106f7b839a0a126
nitish@gaga <nitishgoku01@gmail.com>
2023-07-16 13:08:53 +0530

    adding ports to get spreadlines

--- server.js
+++ server.js
@@ -76,5 +76,5 @@ app.post('/intersect', authenticate, (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log('Server listening on port 3000');
+  console.log('Server listening on port ',PORT);
 });
