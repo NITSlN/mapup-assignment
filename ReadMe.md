7807eeb49350bdef98880f3c294754c3f1c1e949
nitish@gaga <nitishgoku01@gmail.com>
2023-07-16 12:39:55 +0530

    initial commit

---
+++ ReadMe.md
@@ -0,0 +1,60 @@
+# Turf Intersection API
+
+The Turf Intersection API is a Node.js API that allows you to find intersecting lines with a given linestring using the Turf.js library. It provides a simple and efficient way to perform intersection calculations between a linestring and a set of predefined lines.
+
+## Features
+
+- **Line Intersection**: The API can identify which lines intersect with the provided linestring.
+- **Intersection Details**: For each intersecting line, the API returns the line ID and the coordinates of the intersection points.
+- **Authentication**: The API implements authentication using a Bearer token to secure the endpoint.
+
+## API Endpoint
+
+### `POST /intersect`
+
+- Authenticates the request using a Bearer token.
+- Expects a GeoJSON LineString object in the request body.
+- Finds the intersecting lines with the provided linestring.
+- Returns an array of intersecting line IDs along with the coordinates of the intersections.
+
+## Usage
+
+1. Obtain a valid Bearer token for authentication. (For simplicity I have used 1234)
+2. Send a POST request to `/intersect` with the following:
+
+   - Set the `Authorization` header to `Bearer 1234`.
+   - Set the `Content-Type` header to `application/json`.
+   - Include the linestring in the request body as a GeoJSON LineString object.
+
+3. The API will respond with an array of intersecting spread lines with the line IDs and their corresponding intersection coordinates.
+
+## Example Request
+
+http
+POST /intersect
+Content-Type: application/json
+Authorization: Bearer 1234
+
+{
+  "type": "LineString",
+  "coordinates": [
+    [longitude, latitude],
+    ...
+  ]
+}
+
+
+## Example Request
+
+HTTP 200 OK
+Content-Type: application/json
+
+[
+  {
+    "id": "L01",
+    "intersection": [
+      [longitude, latitude],
+    ]
+  },
+  ...
+]
---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const turf = require('@turf/turf');
+const bodyParser = require('body-parser');
+
+let spreadLines = require('./lines.json');
+
+const app = express();
+
+// Parse URL-encoded bodies
+app.use(bodyParser.json());
+
+let index = 0;
+spreadLines = spreadLines.map((sl) => {
+  let id = `L${(index++ + 1).toString().padStart(2, '0')}`;
+  return { id, ...sl };
+});
+
+// Middleware function for header-based authentication
+const authenticate = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Authentication failed. Missing or invalid token.' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(400).json({ message: 'Bad Request. Token is missing.' });
+  }
+
+  // Validate the token here
+  if (token !== '1234') {
+    return res.status(401).json({ message: 'Authentication failed. Invalid token.' });
+  }
+
+  // If authentication succeeds, proceed to the next middleware or route handler
+  next();
+};
+
+// app.get('/', (req, res) => {
+//   res.send(spreadLines)
+// })
+// Route for intersecting the linestring with the lines
+
+app.post('/intersect', authenticate, (req, res) => {
+  // Extract the linestring from the request body
+  const linestring = req.body;
+
+  // Validate the linestring object
+  if (!linestring || linestring.type !== 'LineString' || !linestring.coordinates) {
+    return res.status(400).json({
+      message: 'Invalid linestring. Please provide a valid GeoJSON LineString.',
+    });
+  }
+  let date = new Date()
+  // Filter the spread lines to find the intersecting lines
+  const intersectingLines = spreadLines.filter((spreadLine) => {
+    const spreadLineString = turf.lineString(spreadLine.line.coordinates);
+    return turf.booleanIntersects(spreadLineString, linestring);
+  });
+
+  // Prepare the results with intersecting line IDs and intersection coordinates
+  const results = intersectingLines.map((intersectingLine) => {
+    const spreadLineString = turf.lineString(intersectingLine.line.coordinates);
+    const intersection = turf.lineIntersect(spreadLineString, linestring);
+    return {
+      id: intersectingLine.id,
+      intersection: intersection.features.map((feature) => feature.geometry.coordinates),
+    };
+  });
+  console.log(new Date()-date)
+  // Return the results as JSON response
+  res.json(results);
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
